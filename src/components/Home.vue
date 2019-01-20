8a137b5f588aeeae9dd5ef4a656e50d2b05e110b
DinnerKang <jenhyuk0318@gmail.com>
2019-01-20 23:48:49 +0900

    add home,Login

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="home">
+        <div>
+            <h2>
+                Hi
+            </h2>
+        </div>
+        <div>
+            여기는 홈입니다.
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="login">
+
+    <h1>Login</h1>
+    
+    <form>
+      <div>
+        <label for="user_id">ID</label>
+        <input type="text" id="user_id">
+      </div>
+      <div>
+          <label for="user_pwd">Password</label>
+          <input type="password" id="user_pwd">
+      </div>
+      <div>
+          <button>Login</button>
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+    #login{
+        text-align: center;
+    }
+</style>
